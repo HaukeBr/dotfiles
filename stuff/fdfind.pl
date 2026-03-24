a7d88cc483843afa5df2afb3549dc936e713f446
Your Name <you@example.com>
2026-03-24 02:52:45 +0100

    Reorga

---
+++ stuff/fdfind.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use feature 'say';
+use Data::Dumper;
+
+#my @pkgs = (
+	#	{
+		#		package => {
+			#		name => "fdfind",
+			#	type => "virtual",
+			#versioning => "\d+\.\d+\.\d+",
+			#	depends => []
+			#	}
+		#	},
+
+		
+		#};
+		#
+my $pkg = {
+	package => {
+		name => "fdfind-github",
+	}
